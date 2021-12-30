2b70f94fc35b9dc30c27164e8638f7cfeb8e67a6
Varun Kashyap <20821711+Varun9729@users.noreply.github.com>
2021-12-30 10:28:24 -0800

    Initial Commit

---
+++ Shared/AutoLayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoLayoutsApp.swift
+//  Shared
+//
+//  Created by Varun Kashyap on 12/30/21.
+//
+
+import SwiftUI
+
+@main
+struct AutoLayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
